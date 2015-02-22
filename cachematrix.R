3e044c8bec6fb868077c75a79748c57a503dbb36
SwaatiBohl <bangaloreswaati@gmail.com>
2015-02-22 17:17:41 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two main functions MakeCacheMatrix and cacheSolve. The first main function returns a list of functions namely set,
+##  get, , setinverse, and getinverse. The second main function looks if there exists an inverse matrix and if so returns the 
+## existing (or cached) inverse matrix, else it calculates a new inverse matrix.
 
 ## Write a short comment describing this function
+# This function returns a list of four functions:
+# set returns a matrix that is passed as an argument to makeCacheMatrix or if nothing is passed to makeCacheMatrix
+# then the set function can be used to pass a matrix. e.g : x<-makecacheMatrix() and then x$set(m) where m is a matrix.
+# firthermore set also sets the inverse matrix I to null.
+# get is a function that returns the matrix that has been passed either to makeCacheMatrix or $set()
+# setinverse is a function that can be used to pass the inverse of a passed matrix
+# getinverse is a function that returns the matrix in setinverse, which could be NULL if nothing is passed to setinverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         I <- NULL                               
@@ -20,6 +28,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+#cacheSolve looks for an existing inverse matrix and returns it if found, else it calculates the inverse matrix
+# of teh matrix that was passed to the set function in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
